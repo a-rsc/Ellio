09cfd3d215dd1df7cd27edbf23c76a5c5962d687
Alvaro Rodriguez <a_rsc@hotmail.com>
2021-01-03 17:00:12 +0100

    Update cloud class

--- src/com/jamescho/game/model/Cloud.java
+++ src/com/jamescho/game/model/Cloud.java
@@ -1,2 +1,36 @@
-package com.jamescho.game.model;public class Cloud {
+package com.jamescho.game.model;
+
+import com.jamescho.framework.util.RandomNumberGenerator;
+import com.jamescho.game.main.GameMain;
+import com.jamescho.game.main.Resources;
+
+public class Cloud {
+    private float x;
+    private float y;
+
+    private static final int VEL_X = -15;
+    private static final int CLOUD_RESTART = 200;
+
+    public Cloud(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void update(float delta) {
+        x += VEL_X*delta;
+
+        if(x <= -CLOUD_RESTART) {
+            // Reiniciar a la derecha
+            x += GameMain.GAME_WIDTH + CLOUD_RESTART;
+            y = RandomNumberGenerator.getRandIntBetween(Resources.cloud1.getHeight() / 2, 100);
+        }
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
 }
